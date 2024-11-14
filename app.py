cd5610c999c972968552aef5914d36a4151e7c7d
PreethiUsha08 <preethiusha2002@gmail.com>
2024-11-14 16:04:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def predict_disease(age, sex, albumin, alkaline_phosphatase, alanine_aminotransf
         0: 'Cirrhosis'
     }
     
-    return disease_mapping.get(prediction[0], "Unknown")
+    return disease_mapping.get(prediction[5], "Unknown")
 
 # Streamlit UI elements
 st.title("Disease Prediction")
